3f1001725e9f3560930bdd46019f75ff0a299ee0
shafu.eth <selfouly@gmail.com>
2023-11-18 17:39:26 +0300

    has tokens

--- app.py
+++ app.py
@@ -2,21 +2,11 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Sample data
-tasks = [
-    {
-        'id': 1,
-        'title': 'Task 1',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': 'Task 2',
-        'done': False
-    }
-]
-
 # Route to get all tasks
 @app.route('/games', methods=['GET'])
 def get_tasks():
     return jsonify({'game': "0"})
+
+@app.route('/tokens/<address>', methods=['GET'])
+def get_tokens():
+    return jsonify({'hasToken': "99"})
